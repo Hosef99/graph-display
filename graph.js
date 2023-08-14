499c52cef8447f7ae553c73e22c859d4cbaa7568
Joseph Tan <joseph.txh@gmail.com>
2023-08-14 16:44:25 +0800

    Made graph bidirectional

---
+++ graph.js
@@ -0,0 +1,202 @@
+// sketch.js
+let nodes = [];
+let selectedNode = null;
+
+function createNode() {
+	newNode = new Node(mouseX, mouseY, 30);
+	if (selectedNode != null) {
+		selectedNode.nodes.push(newNode);
+        selectedNode.connectedNodes.push(newNode);
+        newNode.connectedNodes.push(selectedNode);
+        if (!isDirected) {
+            newNode.nodes.push(selectedNode);
+        }
+	}
+	nodes.push(newNode);
+}
+
+function deleteNode() {
+    selectedNode = null;
+		for (let i = nodes.length - 1; i >= 0; i--) {
+			if (nodes[i].contains(mouseX, mouseY)) {
+				nodes[i].delete();
+				nodes.splice(i, 1);
+
+				break;
+			}
+		}
+}
+
+function toggleConnectNode() {
+    if (selectedNode == null) {
+        console.log("No node selected, cannot connect");
+        return;
+    }
+    for (let i = nodes.length - 1; i >= 0; i--) {
+        if (nodes[i].contains(mouseX, mouseY)) {
+            if (selectedNode == nodes[i]) {
+                return;
+            }
+
+            let targetNode = nodes[i];
+            let relation = selectedNode.relation(targetNode);
+            if (relation == 0) {
+                selectedNode.nodes.push(nodes[i]);
+                selectedNode.connectedNodes.push(nodes[i])
+                nodes[i].connectedNodes.push(selectedNode)
+
+                if (!isDirected){
+                    nodes[i].nodes.push(selectedNode);
+                }
+                break;
+            } else if (relation == 1) {
+                let index1Node = selectedNode.findNode(targetNode);
+                let index1Connect = selectedNode.findConnectedNode(targetNode);
+                let index2Connect = targetNode.findConnectedNode(selectedNode);
+                selectedNode.nodes.splice(index1Node, 1)
+                selectedNode.connectedNodes.splice(index1Connect, 1)
+                targetNode.connectedNodes.splice(index2Connect, 1)
+            }
+            else if (relation == -1) {
+                selectedNode.nodes.push(targetNode)
+            }
+            
+            else {
+                let index1 = selectedNode.findNode(targetNode);
+                if (index1 != -1) {
+                    selectedNode.nodes.splice(index1, 1);
+                }
+                if(!isDirected){
+                    let index2 = targetNode.findNode(selectedNode);
+                    if (index2 != -1) {
+                        targetNode.nodes.splice(index2, 1);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Node class
+class Node {
+	constructor(x, y, radius) {
+		this.x = x;
+		this.y = y;
+		this.offsetX = 0;
+		this.offsetY = 0;
+		this.radius = radius;
+		this.nodes = [];
+        this.connectedNodes = []
+	}
+
+	move() {
+		this.x = mouseX - this.offsetX;
+		this.y = mouseY - this.offsetY;
+	}
+
+	addNode(node) {
+		this.nodes.push(node);
+	}
+
+	delete() {
+		for (let i = 0; i < this.connectedNodes.length; i++) {
+			let currNodeList = this.connectedNodes[i].nodes;
+            let currConnectedNodesList = this.connectedNodes[i].connectedNodes;
+			for (let j = 0; j < currNodeList.length; j++) {
+				if (currNodeList[j] == this) {
+					currNodeList.splice(j, 1);
+					break;
+				}
+			}
+            for (let j = 0; j < currConnectedNodesList.length; j++) {
+				if (currConnectedNodesList[j] == this) {
+					currConnectedNodesList.splice(j, 1);
+					break;
+				}
+			}
+
+		}
+	}
+
+	display() {
+		if (this == selectedNode) {
+			fill(0, 255, 0);
+		} else {
+			fill(255, 0, 0);
+		}
+		ellipse(this.x, this.y, this.radius * 2);
+		
+	}
+
+    displayLine() {
+        for (let i = 0; i < this.nodes.length; i++) {
+            let currNode = this.nodes[i]
+			line(this.x, this.y, currNode.x, currNode.y);
+            if (isDirected) {
+                if (currNode.findNode(this) == -1) {
+                    
+                    let midpoint = [(this.x + currNode.x)/2, (this.y + currNode.y)/2]
+                    let angle = Math.atan((currNode.y-this.y)/(currNode.x-this.x))
+                    if (currNode.x < this.x) {
+                        angle += Math.PI
+                    }
+                    drawArrow(midpoint[0], midpoint[1], angle)
+                }
+                else{
+                    let midpoint = [(this.x + currNode.x)/2, (this.y + currNode.y)/2]
+                    let angle = Math.atan((currNode.y-this.y)/(currNode.x-this.x))
+                    if (currNode.x < this.x) {
+                        angle += Math.PI
+                    }
+                    drawArrow(midpoint[0]+Math.cos(angle)*20, midpoint[1]+Math.sin(angle)*20, angle)
+                }
+            }
+		}
+    }
+
+    relation(node) {
+        // -1 -> that node connected to this node 0 -> not connected 1 -> this node connected to that node 2 -> mutually connected
+        let thisIndex = this.findNode(node)
+        let thisConnected = this.findConnectedNode(node)
+        let nodeIndex = node.findNode(this)
+
+        if (thisConnected == -1) {
+            return 0;
+        }
+        else{
+            if (thisIndex == -1) {
+                return -1
+            }
+            else{
+                if (nodeIndex == -1) {
+                    return 1
+                }
+                else{
+                    return 2
+                }
+            }
+        }
+    }   
+
+	contains(x, y) {
+		return dist(x, y, this.x, this.y) < this.radius;
+	}
+
+	findNode(node) {
+		for (let i = 0; i < this.nodes.length; i++) {
+			if (this.nodes[i] == node) {
+				return i;
+			}
+		}
+		return -1;
+	}
+    
+    findConnectedNode(node) {
+        for (let i = 0; i < this.connectedNodes.length; i++) {
+			if (this.connectedNodes[i] == node) {
+				return i;
+			}
+		}
+		return -1;
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-// sketch.js
-let nodes = [];
-let selectedNode = null;
+isDirected = true;
+isDirectedText = "Directed"
+const States = {
+    Text: "text",
+    Normal: "normal"
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    strokeWeight(5)
 }
 
 function windowResized() {
@@ -12,12 +16,17 @@ function windowResized() {
 
 function draw() {
     background(220);
-
+    fill(0)
+    text(isDirectedText, 30,30)
     drawNodes();
+    
 }
 
 function drawNodes() {
     // Draw all the nodes
+    for (let node of nodes) {
+        node.displayLine();
+    }
     for (let node of nodes) {
         node.display();
     }
@@ -45,111 +54,15 @@ function mousePressed() {
 function keyPressed() {
     // Create
     if (key === "q") {
-        newNode = new Node(mouseX, mouseY, 30)
-        if (selectedNode != null) {
-            newNode.nodes.push(selectedNode)
-            selectedNode.nodes.push(newNode)
-        }
-        nodes.push(newNode);
+        createNode()
     }
     // Delete
     if (key === "d") {
-        selectedNode = null;
-        for (let i = nodes.length - 1; i >= 0; i--) {
-            if (nodes[i].contains(mouseX, mouseY)) {
-                nodes[i].delete()
-                nodes.splice(i, 1);
-
-                break;
-            }
-        }
+        deleteNode()
     }
     // Connect / Disconnect
     if (key === "w") {
-        if (selectedNode == null) {
-            console.log("No node selected, cannot connect")
-            return
-        }
-        for (let i = nodes.length - 1; i >= 0; i--) {
-            if (nodes[i].contains(mouseX, mouseY)) {
-                if (selectedNode == nodes[i]) {
-                    return
-                }
-                let targetNode = nodes[i] 
-                if (!nodes[i].nodes.includes(selectedNode)) {
-                    selectedNode.nodes.push(nodes[i])
-                    nodes[i].nodes.push(selectedNode)
-                    break;
-                }
-                else{
-                    let index1 = selectedNode.findNode(targetNode)
-                    let index2 = targetNode.findNode(selectedNode)
-                    if(index1 != -1 && index2 != -1){
-                        selectedNode.nodes.splice(index1, 1)
-                        targetNode.nodes.splice(index2, 1)
-                    }
-                }
-            }
-        }
-    }
-}
-
-// Node class
-class Node {
-    constructor(x, y, radius) {
-        this.x = x;
-        this.y = y;
-        this.offsetX = 0
-        this.offsetY = 0
-        this.radius = radius;
-        this.nodes = []
-    }
-
-    move() {
-        this.x = mouseX - this.offsetX;
-        this.y = mouseY - this.offsetY;
-    }
-
-    addNode(node) {
-        this.nodes.push(node);
-    }
-
-    delete() {
-        for (let i = 0; i < this.nodes.length; i++) {
-            let currNodeList = this.nodes[i].nodes
-            for (let j = 0; j < currNodeList.length; j++) {
-                if (currNodeList[j] == this) {
-                    currNodeList.splice(j, 1);
-                    break;
-                }
-            }
-        }
-    }
-
-    display() {
-        if (this == selectedNode) {
-            fill(0, 255, 0);
-        }
-        else {
-            fill(255, 0, 0);
-        }
-        ellipse(this.x, this.y, this.radius * 2);
-        for (let i = 0; i < this.nodes.length; i++) {
-            line(this.x, this.y, this.nodes[i].x, this.nodes[i].y);
-        }
-    }
-
-    contains(x, y) {
-        return dist(x, y, this.x, this.y) < this.radius;
-    }
-
-    findNode(node) {
-        for (let i = 0; i < this.nodes.length; i++) {
-            if (this.nodes[i] == node) {
-                return i
-            }
-        }
-        return -1;
+        toggleConnectNode()
     }
 }
 
@@ -159,3 +72,27 @@ function mouseDragged() {
     }
     selectedNode.move()
 }
+
+function toggleDirected() {
+    isDirected = !isDirected
+    isDirectedText = isDirected ? "Directed" : "Not Directed" 
+}
+
+
+// Mathematical calculations
+
+function degToRad(angle) {
+    return angle * (Math.PI / 180.0);
+}
+
+function rotatePoint(x, y, angle) {
+    return [10*parseFloat((x*Math.cos(angle)+y*Math.sin(angle))), 10*parseFloat((-x*Math.sin(angle)+y*Math.cos(angle)))]
+}
+
+function drawArrow(x, y, angle) {
+    point1 = rotatePoint(-1/Math.sqrt(2), -1/Math.sqrt(2), -angle)
+    point2 = rotatePoint(-1/Math.sqrt(2), 1/Math.sqrt(2), -angle)
+    line(point1[0] + x, point1[1] + y, x, y)
+    line(point2[0] + x, point2[1] + y, x, y)
+}
+
